8a9a0b272863437ddc36f0abb087dd2cfc03b376
Nogic <24802730+nogic1008@users.noreply.github.com>
2023-09-15 01:36:10 +0000

    feat: move files to simplify aside & toc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,5 @@ export default defineNuxtConfig({
   extends: '@nuxt-themes/docus',
   nitro: { preset: 'github_pages' },
   modules: ['@nuxt/devtools'],
+  content: { markdown: { toc: { depth: 3 } } },
 })
