4ad73921aa11bb201f00ad1e7ef32e39c46b78a3
Nogic <24802730+nogic1008@users.noreply.github.com>
2023-09-26 05:10:16 +0000

    build: add static option

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 export default defineNuxtConfig({
   extends: '@nuxt-themes/docus',
   nitro: { preset: 'github_pages' },
+  routeRules: {
+    '/**': { static: true },
+  },
   modules: ['@nuxt/devtools'],
   content: { markdown: { toc: { depth: 3 } } },
 })
