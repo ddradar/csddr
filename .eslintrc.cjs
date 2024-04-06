b312a913cb6051f9e34c3d59c7dd7f3ad7936c21
Nogic <24802730+nogic1008@users.noreply.github.com>
2024-04-06 11:25:07 +0000

    chore(lint): migrate to eslint flat config

--- .eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  extends: ['@nuxt/eslint-config', 'prettier'],
-  rules: {
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    'vue/max-attributes-per-line': 'off',
-    'vue/multi-word-component-names': 'off',
-  },
-}
---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt({
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_' },
+    ],
+  },
+})
