e7f661c0fdc51e1f7e3155676882873dde4cd355
Nogic <24802730+nogic1008@users.noreply.github.com>
2024-04-07 14:43:27 +0900

    feat: add top page link (#172)

--- pages/index.vue
+++ pages/index.vue
@@ -19,13 +19,28 @@ defineOgImage({
 
 <template>
   <div>
-    <ULandingSection :title="page.features.title" :links="page.features.links">
+    <ULandingSection
+      v-for="feature in page.features"
+      :key="feature.title"
+      :title="feature.title"
+      :ui="{
+        wrapper: 'py-8 sm:py-12',
+        container: 'gap-4 sm:gap-y-8',
+        title: 'text-2xl sm:text-3xl lg:text-4xl',
+      }"
+    >
       <UPageGrid>
         <ULandingCard
-          v-for="(item, index) of page.features.items"
+          v-for="(item, index) of feature.items"
           :key="index"
           v-bind="item"
-        />
+        >
+          <template #description>
+            <!-- eslint-disable-next-line vue/no-v-html -->
+            <span v-html="item.description"></span>
+            <time class="block text-right text-gray-600">{{ item.date }}</time>
+          </template>
+        </ULandingCard>
       </UPageGrid>
     </ULandingSection>
   </div>
