3c09086b9c7c6dd80dd15d1a2ae305b3897f8831
Nogic <24802730+nogic1008@users.noreply.github.com>
2025-02-09 10:56:33 +0900

    fix: fix Edit this page link

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -47,7 +47,7 @@ const links = computed(() =>
     toc?.bottom?.edit && {
       icon: 'i-lucide-external-link',
       label: 'Edit this page',
-      to: `${toc.bottom.edit}/${page?.value?.path}`,
+      to: `${toc.bottom.edit}/${page?.value?.stem}.${page?.value?.extension}`,
       target: '_blank',
     },
   ].filter(Boolean)
