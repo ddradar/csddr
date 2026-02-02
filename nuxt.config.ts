ff908dad3cda12b9733f2f4469fd16892aa9c787
Nogic <24802730+nogic1008@users.noreply.github.com>
2026-02-02 11:41:14 +0900

    chore(deps): update project dependencies (#424)
    * chore: use Node.js native sqlite feature
    
    * chore(deps): update project dependencies
    
    * chore: use pnpm run & remove npm-run-all2 package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   nitro: { preset: 'github-pages' },
-  icon: {
-    provider: 'iconify',
+  content: {
+    experimental: { sqliteConnector: 'native' },
   },
 })
