279a58e92a71bdd45caf2767175fe8f12202b061
Nogic <24802730+nogic1008@users.noreply.github.com>
2026-02-06 11:46:13 +0900

    perf: optimize build config (#431)
    * chore(deps): add @nuxtjs/mdc
    
    * build: use some build config
    
    * fix: remove option

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,17 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-07-11',
   modules: ['@nuxt/ui', '@nuxt/content', '@nuxt/eslint', '@nuxt/image'],
-  devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  experimental: {
+    extractAsyncDataHandlers: true,
+    typedPages: true,
+    typescriptPlugin: true,
+  },
   nitro: { preset: 'github-pages' },
+  vite: {
+    vue: { features: { optionsAPI: false } },
+  },
+  css: ['~/assets/css/main.css'],
+  devtools: { enabled: true },
   content: {
     experimental: { sqliteConnector: 'native' },
   },
