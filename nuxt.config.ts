f7c44fa8f78026db395c21d5b5d60d9366d029b4
Nogic <24802730+nogic1008@users.noreply.github.com>
2026-02-06 11:30:07 +0900

    build: use some build config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,20 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-07-11',
   modules: ['@nuxt/ui', '@nuxt/content', '@nuxt/eslint', '@nuxt/image'],
-  devtools: { enabled: true },
+  experimental: {
+    extractAsyncDataHandlers: true,
+    typedPages: true,
+    typescriptPlugin: true,
+  },
+  nitro: {
+    preset: 'github-pages',
+    compressPublicAssets: true,
+  },
+  vite: {
+    vue: { features: { optionsAPI: false } },
+  },
   css: ['~/assets/css/main.css'],
-  nitro: { preset: 'github-pages' },
+  devtools: { enabled: true },
   content: {
     experimental: { sqliteConnector: 'native' },
   },
