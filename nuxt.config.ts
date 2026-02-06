37721f10da8e15969a451f63815bd65f324133f0
Nogic <24802730+nogic1008@users.noreply.github.com>
2026-02-06 11:42:18 +0900

    fix: remove option

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,10 +6,7 @@ export default defineNuxtConfig({
     typedPages: true,
     typescriptPlugin: true,
   },
-  nitro: {
-    preset: 'github-pages',
-    compressPublicAssets: true,
-  },
+  nitro: { preset: 'github-pages' },
   vite: {
     vue: { features: { optionsAPI: false } },
   },
