3d3e00331c32222d4718500924976f97436bd31c
Nogic <24802730+nogic1008@users.noreply.github.com>
2026-03-05 11:57:34 +0900

    chore: remove eslint-config-prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,8 @@
 // @ts-check
-import prettier from 'eslint-config-prettier'
 import oxlint from 'eslint-plugin-oxlint'
 
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  prettier,
   ...oxlint.buildFromOxlintConfigFile('./.oxlintrc.json')
 )
